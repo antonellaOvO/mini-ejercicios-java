fda08ec5ffc6afe634a954593b22f54dba6f405f
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2024-04-23 15:48:06 +0200

    added exercise 1- Hola Mundo

--- Hola-Mundo/src/hola/mundo/HolaMundo.java
+++ Hola-Mundo/src/hola/mundo/HolaMundo.java
@@ -15,6 +15,11 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola Mundo");
+        Usuario usuario1 = new Usuario("Antonella");
+        usuario1.saludar("Antonella");
+        usuario1.preguntar("Antonella");
+        usuario1.repetirSaludo(3, "Ella");
     }
     
 }
---
+++ Hola-Mundo/src/hola/mundo/Usuario.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hola.mundo;
+
+/**
+ *
+ * @author anto_
+ */
+public class Usuario {
+    public String nombre;
+    /**
+     * @param args the command line arguments
+     */public Usuario(String nombreUsuario) {
+         this.nombre = nombreUsuario;
+     }
+     
+     public void saludar(String nombreUsuario) {
+         System.out.println("Hola " + nombreUsuario + "!");
+     }
+     public void preguntar(String nombreUsuario) {
+         System.out.println("Hola " + nombreUsuario + "!" + " Como estas " + nombreUsuario + "?");
+     }
+     public void repetirSaludo(int numero, String nombreUsuario) {
+         int j = 1;
+         while(j < numero) {
+             System.out.println("Hola " + nombreUsuario + "!");
+             j++;
+         }
+         System.out.println("Hola " + nombreUsuario + "!");
+     }
+     
+    
+    
+}
